281bd172d44521fece714d413e9d1ae697c2b2ff
Vinh Tran <trvinh@Vinhs-MacBook-Pro.local>
2016-10-18 10:14:33 +0200

    setup file for packages installing

---
+++ installPackages.R
@@ -0,0 +1,12 @@
+libs=c("shiny","shinyBS","ggplot2","reshape","plyr","dplyr","scales‌​","grid","gridExtra","ape")
+type=getOption("pkgType")                           
+    CheckInstallPackage <- function(packages, repos="http://cran.r-project.org",
+       depend=c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances"), ...) {
+         installed=as.data.frame(installed.packages())
+    for(p in packages) {
+        if(is.na(charmatch(p, installed[,1]))) { 
+          install.packages(p, repos=repos, dependencies=depend, ...) 
+                    }
+      }
+    } 
+    CheckInstallPackage(packages=libs)
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#PBS -N qsub_R_test
-
-cd $PBS_O_WORKDIR
-echo installing shinyPackage ...
-
-R CMD BATCH install.R
