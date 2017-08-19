737f24b678138c95b1b31d3d2b34daa891f79225
trvinh <tran@bio.uni-frankfurt.de>
2017-08-20 00:07:38 +0200

    new install script

--- installPackages.R
+++ installPackages.R
@@ -5,7 +5,7 @@
 ipak <- function(pkg){
     new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
     if (length(new.pkg))
-        install.packages(new.pkg, dependencies = TRUE)
+        install.packages(new.pkg, dependencies = TRUE, repos="http://cran.us.r-project.org")
     sapply(pkg, require, character.only = TRUE)
 }
 
