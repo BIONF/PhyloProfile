28140151aa84fed7c2460674ac4eef55b70e6e6e
Bastian Greshake <bgreshake@googlemail.com>
2017-09-28 18:15:52 +0200

    added simple OMA-XML-Downloader

---
+++ scripts/get_oma_hogs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import argparse
+import urllib
+from cStringIO import StringIO
+from lxml import etree
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--ids', nargs='+',
+                    help='one or more OMA protein-IDs that will be downloaded.\
+                    Multiple IDs are separated by spaces.\
+                    (required)',required=True)
+args = parser.parse_args()
+
+def read_xml(ID):
+    try:
+        oma_url = urllib.urlopen("http://omabrowser.org/oma/hogs/%s/orthoxml/" % ID)
+        oma_xml = oma_url.read()
+        oma_xml_tree = etree.parse(StringIO(oma_xml))
+        return oma_xml_tree
+    except:
+        print("------------------------")
+        print("Could not find %s in OMA" % (ID))
+        print("Are you sure that this is a valid protein ID?")
+        exit()
+
+
+
+def merge_xml(oma1,oma2):
+    oma1_root = oma1.getroot()
+    oma2_root = oma2.getroot()
+    ns = "{"+oma1_root.nsmap[None]+"}"
+    # iterate over all species in file #2
+    for species in oma2.iter(ns+"species"):
+        # get taxonomy id
+        tax_id_oma2 = species.attrib["NCBITaxId"]
+        # try finding taxon in file #1
+        taxon_oma1 = oma1_root.find("{http://orthoXML.org/2011/}species[@NCBITaxId='"+tax_id_oma2+"']")
+        # file #1 has the taxon from file #2, append all genes
+        if taxon_oma1 != None:
+            taxon_oma1_genes = taxon_oma1.find(ns+"database").find(ns+"genes")
+            taxon_oma2_genes = species.find(ns+"database").find(ns+"genes")
+            for i in taxon_oma2_genes.getchildren():
+                taxon_oma1_genes.append(i)
+        # meh, have to add the whole species to it.
+        else:
+            oma1_root.findall(ns+"species")[-1].addnext(species)
+
+    for group in oma2.find(ns+"groups").getchildren():
+        oma1.find(ns+"groups").getchildren()[-1].addnext(group)
+    return oma1
+
+
+#oma1 = read_xml("RATNO03709")
+#oma2 = read_xml("RATNO03710")
+
+merged_id = args.ids[0]
+merged_xml = read_xml(merged_id)
+
+other_ids = args.ids[1:]
+for single_id in other_ids:
+    xml_to_append = read_xml(single_id)
+    merged_xml = merge_xml(merged_xml,xml_to_append)
+
+print etree.tostring(merged_xml)
