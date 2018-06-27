2e05b97458fd84a891b6ddea373b954777c40065
CarlaMoelbert <35259165+CarlaMoelbert@users.noreply.github.com>
2018-06-27 10:34:26 +0200

    Modularizing  Group Comparison  (#82)
    * Revert "Group comparison:  Last changes on the Group-Comparison function"
    
    * Update CONTRIBUTING.md
    
    * fixed bug #49 and #55
    
    * Update CONTRIBUTING.md
    
    * Update CONTRIBUTING.md
    
    * Update README.md
    
    * Update README.md
    
    * Update CONTRIBUTING.md
    
    * Update README.md
    
    * Added fisher, pearson, mutual information and distance correlation as distance measueres
    
    * The new functions work know in profiles clustering
    
    * The genes can now be clustered with the new methods in the heat map
    
    * Clustering in the head map works now with non-binary profiles as well (always non-binary when mutual information or distance correlation are selected)
    
    * After pulling from modularizing branch
    
    * started generating group comparison module
    
    * Adding Group Comparison module. Error message in scripts/functions.R
    
    * added function explanetions to module group comparison
    
    * added comments to module group comparison
    
    * download function in module group comparison is now working

--- R/cluster_profile2.R
+++ R/cluster_profile2.R
@@ -1,5 +1,16 @@
 #' Profile clustering
 #'
+#' @param data
+#' @param dist_method
+#' @param cluster_method
+#' @param cluster_plot.width
+#' @param cluster_plot.height
+#' @param var1_aggregate_by
+
+if (!require("bioDist")) install.packages("bioDist") # for the mutual information
+if (!require("energy")) install.packages("energy") # for the mutual information, pearson
+
+source("R/functions.R")
 
 cluster_profile_ui <- function(id){
   ns <- NS(id)
@@ -23,19 +34,23 @@ cluster_profile_ui <- function(id){
 cluster_profile <- function(input, output, session,
                             data,
                             dist_method, cluster_method,
-                            cluster_plot.width, cluster_plot.height){
+                            cluster_plot.width, cluster_plot.height,
+                            var1_aggregate_by){
   
   cluster_data <- reactive({
-    if (is.null(data())) return()
-    df <- clusterDataDend(data(), dist_method(), cluster_method())
+    df <- clusterDataDend(data(),
+                          dist_method(),
+                          cluster_method(),
+                          var1_aggregate_by())
+
     return(df)
   })
-  
+
   output$dendrogram <- renderPlot({
     if (is.null(data())) return()
     dendrogram(cluster_data())
   })
-  
+
   output$cluster.ui <- renderUI({
     ns <- session$ns
     withSpinner(
@@ -51,7 +66,7 @@ cluster_profile <- function(input, output, session,
       )
     )
   })
-  
+
   # download clustered plot ---------------------------------------------------
   output$download_cluster <- downloadHandler(
     filename = function() {
@@ -63,38 +78,38 @@ cluster_profile <- function(input, output, session,
              limitsize = FALSE)
     }
   )
-  
+
   # render brushed_cluster.table based on clicked point on dendrogram plot ----
   brushed_clusterGene <- reactive({
     # if (v$doPlot == FALSE) return()
-    
+
     dd.col <- cluster_data()
     dt <- dendro_data(dd.col)
     dt$labels$label <- levels(dt$labels$label)
-    
+
     # get list of selected gene(s)
     if (is.null(input$plot_brush)) return()
     else{
       top <- as.numeric(-round(input$plot_brush$ymin))
       bottom <- as.numeric(-round(input$plot_brush$ymax))
-      
+
       df <- dt$labels[bottom:top, ]
     }
-    
+
     # return list of genes
     df <- df[complete.cases(df), 3]
   })
-  
+
   output$brushed_cluster.table <- renderTable({
     if (is.null(input$plot_brush$ymin)) return()
-    
+
     data <- as.data.frame(brushed_clusterGene())
     data$number <- rownames(data)
     colnames(data) <- c("geneID", "No.")
     data <- data[, c("No.", "geneID")]
     data
   })
-  
+
   # download gene list from brushed_cluster.table -----------------------------
   output$download_cluster_genes <- downloadHandler(
     filename = function(){
@@ -105,35 +120,80 @@ cluster_profile <- function(input, output, session,
       write.table(data_out, file, sep = "\t", row.names = FALSE, quote = FALSE)
     }
   )
-  
   return(brushed_clusterGene)
 }
 
-# cluster data --------------------------------------------------------------
-clusterDataDend <- function(data, dist_method, cluster_method){
+
+#' cluster data ----------------------------------------------------------------
+#' @export
+#' @param data
+#' @param dist_method
+#' @param cluster_method
+#' @param var1_aggregate_by
+#' @return new data frame with % of present species
+#' @author Vinh Tran {tran@bio.uni-frankfurt.de}
+
+clusterDataDend <- function(data,
+                            dist_method,
+                            cluster_method,
+                            var1_aggregate_by){
   # if (v$doPlot == FALSE) return()
   # dataframe for calculate distance matrix
-  dataHeat <- data
-  
-  sub_data_heat <- subset(dataHeat, dataHeat$presSpec > 0)
-  sub_data_heat <- sub_data_heat[, c("geneID", "supertaxon", "presSpec")]
-  sub_data_heat <- sub_data_heat[!duplicated(sub_data_heat), ]
-  
-  wide_data <- spread(sub_data_heat, supertaxon, presSpec)
-  dat <- wide_data[, 2:ncol(wide_data)]  # numerical columns
-  rownames(dat) <- wide_data[, 1]
-  dat[is.na(dat)] <- 0
-  
-  dd.col <- as.dendrogram(hclust(dist(dat, method = dist_method),
+  dat <- get_data_clustering(data, dist_method, var1_aggregate_by)
+  dd.col <- as.dendrogram(hclust(get_distance_matrix(dat, dist_method),
                                  method = cluster_method))
-  
   return(dd.col)
 }
 
-# plot clustered profiles -----------------------------------------------------
+#' plot clustered profiles -----------------------------------------------------
+#' @export
+#' @param dd.col
+#' @return plot clustered profiles
+#' @author Vinh Tran {tran@bio.uni-frankfurt.de}
+
 dendrogram <- function(dd.col){
   py <- as.ggdend(dd.col)
   p <- ggplot(py, horiz = TRUE, theme = theme_minimal()) +
     theme(axis.title = element_blank(), axis.text.y = element_blank())
-  p
+  return(p) 
+}
+
+
+#' Get the distance matrix depending on the distance method---------------------
+#' @export
+#' @param profiles datafram containing phylogenetic profiles
+#' @param dist_method distance method
+#' @return distance matrix
+#' @author Carla Mölbert (carla.moelbert@gmx.de)
+get_distance_matrix <- function(profiles, method){
+  dist_methods <- c("euclidean", "maximum", "manhattan", "canberra", "binary")
+  if (method %in% dist_methods) {
+    distance_matrix <- dist(profiles, method = method)
+  } else if (method %in% c("fisher", "distance_correlation")) {
+    matrix <- data.frame()
+    for (i in 1:nrow(profiles)) { # rows
+      for (j in 1:nrow(profiles)) { # columns
+        if (i == j) {
+          matrix[i,i] = 1 # if this cell is NA as.dist does not work probably 
+          break
+        }
+        if (method == "fisher") {
+          contigency_table <- get_contengency_table(profiles[i,], profiles[j,])
+          dist <- fisher.test(contigency_table)
+        } else if (method == "distance_correlation") {
+          dist <- dcor(unlist(profiles[i,]), unlist(profiles[j,]))
+        }
+        matrix[i,j] <- dist 
+      }
+    }
+    profile_names <- rownames(profiles)
+    colnames(matrix) <- profile_names[1:length(profile_names) - 1]
+    rownames(matrix) <- profile_names
+    distance_matrix <- as.dist(matrix)
+  } else if (method == "mutual_information") {
+    distance_matrix <- mutualInfo(as.matrix(profiles))
+  } else if (method == "pearson") {
+    distance_matrix <-  cor.dist(as.matrix(profiles))
+  }
+  return(distance_matrix)
 }
