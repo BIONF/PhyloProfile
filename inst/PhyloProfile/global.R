4a87fd55d03d976712b91bc2c76c59794e21683f
trvinh <tran@bio.uni-frankfurt.de>
2019-07-19 11:31:09 +0200

    modified global.R

--- inst/PhyloProfile/global.R
+++ inst/PhyloProfile/global.R
@@ -5,30 +5,32 @@
 source("R/functions.R")
 
 # List of dependent packages --------------------------------------------------
-packages <- c("ape", "colourpicker", "data.table",
-              "plyr", "DT", "energy", "ggplot2", "gplots",
-              "grid", "gridExtra", "gtable", "RCurl", "RColorBrewer",
-              "reshape2", "scales", "shiny", "shinyBS", "shinyjs",
-              "tidyr", "zoo")
+packages <- c(
+    "ape", "bioDist", "Biostrings", "colourpicker", "data.table", "energy", 
+    "GenomeInfoDbData", "ggplot2", "GO.db", "grid", "gridExtra", "RColorBrewer",
+    "shiny", "shinyBS", "shinyjs", "OmaDB", "zoo"
+)
 
 # Find & install missing packages ---------------------------------------------
-installPackages(packages)
+# installPackages(packages)
 
 # Load packages
 lapply(packages, library, character.only = TRUE)
 
 # Install packages from bioconductor ------------------------------------------
-bioconductor_pkgs <- c("Biostrings", "bioDist")
-installPackagesBioconductor(bioconductor_pkgs)
-lapply(bioconductor_pkgs, library, character.only = TRUE)
+# bioconductor_pkgs <- c("Biostrings", "bioDist")
+# installPackagesBioconductor(bioconductor_pkgs)
+# lapply(bioconductor_pkgs, library, character.only = TRUE)
 
 # Install OmaDB's dependencies "GO.db", "GenomeInfoDbData"
 # and ExperimentHub
-oma_pkgs <- c("GO.db", "GenomeInfoDbData", "ExperimentHub")
-installPackagesBioconductor(oma_pkgs)
+# oma_pkgs <- c("GO.db", "GenomeInfoDbData", "ExperimentHub")
+# installPackagesBioconductor(oma_pkgs)
+if (!requireNamespace("ExperimentHub"))
+    BiocManager::install("ExperimentHub")
 if (packageVersion("ExperimentHub") < "1.11.1")
     BiocManager::install(pkgs = "ExperimentHub", version = "devel")
-lapply(oma_pkgs, library, character.only = TRUE)
+library(ExperimentHub)
 
 # Load demo data from PhyloProfileData package
 eh = ExperimentHub()
