85d76ee14b4b9ef0d89b132e5b195b552ba47c08
trvinh <trvinh@gmail.com>
2020-05-29 13:03:00 +0200

    Update R version

--- inst/PhyloProfile/ui.R
+++ inst/PhyloProfile/ui.R
@@ -127,7 +127,7 @@ shinyUI(
 
         # MAIN NARVARPAGE TABS -------------------------------------------------
         navbarPage(
-            em(strong("PhyloProfile v1.2.4")),
+            em(strong("PhyloProfile v1.2.5")),
             id = "tabs",
             collapsible = TRUE,
             inverse = TRUE,
