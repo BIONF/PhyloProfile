37bf7d16461a67003a7403cec45daec3b5b69b93
trvinh <trvinh@gmail.com>
2023-01-02 14:30:55 +0100

    fixed bug customized profile not showed

--- inst/PhyloProfile/ui.R
+++ inst/PhyloProfile/ui.R
@@ -139,7 +139,7 @@ shinyUI(
 
         # MAIN NARVARPAGE TABS -------------------------------------------------
         navbarPage(
-            em(strong("PhyloProfile v1.12.1")),
+            em(strong("PhyloProfile v1.12.3")),
             id = "tabs",
             collapsible = TRUE,
             inverse = TRUE,
