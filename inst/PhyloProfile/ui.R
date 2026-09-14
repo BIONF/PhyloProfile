6833fafde375215a7b30924b8c1aeaaf6fcc8591
trvinh <trvinh@gmail.com>
2026-09-14 18:10:04 +0200

    Change Vinh email

--- inst/PhyloProfile/ui.R
+++ inst/PhyloProfile/ui.R
@@ -161,7 +161,7 @@ shinyUI(
 
         # MAIN NARVARPAGE TABS -------------------------------------------------
         navbarPage(
-            em(strong("PhyloProfile v2.4.6")),
+            em(strong("PhyloProfile v2.4.7")),
             id = "tabs",
             collapsible = TRUE,
             inverse = TRUE,
